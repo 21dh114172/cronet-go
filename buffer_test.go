4c20c621b7432c3a24d09fd908ca125414a7fb02
Sleeyax <yourd3veloper@gmail.com>
2023-10-15 20:41:06 +0200

    Update buffer test

--- buffer_test.go
+++ buffer_test.go
@@ -2,22 +2,25 @@ package cronet_test
 
 import (
 	"bytes"
+	"github.com/sagernet/cronet-go"
+	"github.com/sagernet/cronet-go/utils"
+	"github.com/sagernet/sing/common/buf"
 	"sync"
 	"testing"
-
-	"github.com/sagernet/cronet-go"
-	"github.com/sagernet/sing/common/buf"
-	"github.com/sagernet/sing/common/random"
 )
 
 func TestBuffer(t *testing.T) {
+	randomBytes, err := utils.GetRandomBytes()
+	if err != nil {
+		t.Fatal(err)
+	}
 	b := cronet.NewBuffer()
 	b.InitWithAlloc(1024)
 	if b.Size() != 1024 {
 		t.Fatal("bad size")
 	}
 	data := buf.StackNewSize(1024)
-	data.ReadFullFrom(random.Default, 1024)
+	data.ReadFullFrom(randomBytes, 1024)
 	copy(b.DataSlice(), data.Bytes())
 	if bytes.Compare(b.DataSlice(), data.Bytes()) != 0 {
 		t.Fatal("bad data")
@@ -26,9 +29,13 @@ func TestBuffer(t *testing.T) {
 }
 
 func TestManagedBuffer(t *testing.T) {
+	randomBytes, err := utils.GetRandomBytes()
+	if err != nil {
+		t.Fatal(err)
+	}
 	data := buf.StackNewSize(1024)
-	data.ReadFullFrom(random.Default, 1024)
-	dataCopy := data.Copy()
+	data.ReadFullFrom(randomBytes, 1024)
+	dataCopy := utils.CopyBuffer(data)
 	b := cronet.NewBuffer()
 	var wg sync.WaitGroup
 	wg.Add(1)
